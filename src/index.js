d09dc94d4ba4d2da06e38e42c18bf35d49b72817
marinrika <marinrika2016@gmail.com>
2023-01-09 18:24:21 +0300

    feat: test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    let a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let c = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let result = '';
+    num = String(num);
+    num == 0 ? result += 'zero' :
+        num.length == 1 ? result += a[num] : 
+        num.length == 2 & num.slice(0, 1) == 1 ? result += c[num.slice(-1)] :
+        num.length == 2 & num.slice(-1) == 0 ? result += b[num.slice(0, 1)] :
+        num.length == 2 & num.slice(0, 1) !== 1 & num.slice(-1) !== 0? result += b[num.slice(0, 1)] + ' ' + a[num.slice(-1)] : 
+        num.length == 3 & num.slice(1, 2) == 1 ? result += a[num.slice(0, 1)] + ' hundred' + ' ' + c[num.slice(-1)] :
+        num.length == 3 & num.slice(1, 2) == 0 & num.slice(-1) == 0 ? result += a[num.slice(0, 1)] + ' hundred' :
+        num.length == 3 & num.slice(1, 2) == 0 & num.slice(-1) !== 0 ? result += a[num.slice(0, 1)] + ' hundred' + ' ' + a[num.slice(-1)]:
+        result += a[num.slice(0, 1)] + ' hundred' + ' ' + b[num.slice(1, 2)] + ' ' + a[num.slice(-1)];
+    return result.trim();
 }
